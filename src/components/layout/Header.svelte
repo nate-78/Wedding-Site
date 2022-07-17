4746ac4d20ece525edcb57d31487424bcd775a8a
Nathan Owens <nowens@ccgideas.com>
2022-07-17 14:38:24 -0500

    minor updates to header

---
+++ src/components/layout/Header.svelte
@@ -0,0 +1,38 @@
+<header>
+  <div class="container">
+    <div class="row">
+      <div class="logo">
+        Lindsay + Nathan
+      </div>
+
+      <nav>
+        <ul>
+          <li><a href="#when-and-where">When &amp; Where</a></li>
+          <li><a href="#rsvp">RSVP</a></li>
+        </ul>
+      </nav>
+    </div>
+  </div>
+</header>
+
+
+<style>
+  header {
+    padding: 1rem 0;
+  }
+  ul {
+    list-style: none;
+    display: flex;
+    justify-content: flex-end;
+    margin: 0;
+    padding: 0;
+  }
+  ul li {
+    margin-left: 1rem;
+    font-size: 16px;
+  }
+
+  .row {
+    align-items: center;
+  }
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,15 @@
 <script>
-  
+  import Header from "../components/layout/Header.svelte";
+  import Footer from "../components/layout/Footer.svelte";
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<Header />
+
+<div class="top">
+  <div class="container">
+    <h1>Welcome to SvelteKit</h1>
+    <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+  </div>
+</div>
+
+<Footer />
